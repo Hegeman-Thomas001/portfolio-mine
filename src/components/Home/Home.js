287e55cb923a01d5046db75a9082bef4091e416a
Thomas Hegeman <hegemant001@gmail.com>
2023-01-24 15:06:02 -0500

    added Home colors and text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 //
 const Home = () => {
   return (
-    <section id="home" className="bg-blue-800  h-screen">
-      <h1>Home</h1>
+    <section id="home" className="bg-black  h-screen flex justify-center items-center">
+      <section className="font-bold text-4xl lg:text-6xl text-slate-200 text-center">
+        <h2>Hello, I am <span className="text-red-600">Thomas</span>,</h2>
+        <h2>a software engineer.</h2>
+      </section>
     </section>
   );
 };
