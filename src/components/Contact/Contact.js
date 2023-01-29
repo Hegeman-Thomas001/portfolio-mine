fead2db611a8db7fee05c3861204d20249c18bfe
Thomas Hegeman <hegemant001@gmail.com>
2023-01-29 14:16:35 -0500

    added react social icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,13 @@
+import { SocialIcon } from "react-social-icons";
 //
 const Contact = () => {
   return (
-    <section id="contact" className="bg-red-800  h-screen">
+    <section id="contact" className="bg-black h-screen">
       <h1>Contact</h1>
+      <SocialIcon url="https://twitter.com/TomHegemanDev" />
+      <SocialIcon url="https://www.linkedin.com/in/thomas-hegeman-dev/" />
+      <SocialIcon url="https://github.com/Hegeman-Thomas001" />
+      <SocialIcon url="" />
     </section>
   );
 };
